b4f8f2cb3a0bd11488a3acf200a6b5cef2741ebc
Simon Wang <simon.wang@emc.com>
2016-05-09 17:24:59 -0400

    Remove the step to start and stop server in unit test

--- fiboservice/src/main/java/myapp/fibo/myapp/fibo/demo/FiboAyncDemo.java
+++ fiboservice/src/main/java/myapp/fibo/myapp/fibo/demo/FiboAyncDemo.java
@@ -10,7 +10,7 @@ public class FiboAyncDemo {
     public static void main(String[] args) throws Exception {
         System.out.println("Usage: get_fibo.sh sn pathToResultFile");
         System.out.println("Example: get_fibo.sh 100 ./result");
-        
+
         if (args.length < 2) {
             System.exit(1);
         }
