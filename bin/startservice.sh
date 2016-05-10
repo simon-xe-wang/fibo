ecde847606a1459ec3af151689e50c872c351da3
Simon Wang <simon.wang@emc.com>
2016-05-10 22:52:45 +0000

    fix log issues

--- bin/startservice.sh
+++ bin/startservice.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 export CLASSPATH=../conf:$(find ../lib/ -name *.jar |tr "\n" ":")
 
-java -server -d64 -Xmx1G -Dlog4j.configuration=log4j.properties myapp.fibo.service.Main &
+java -server -d64 -Xmx1G -Dlog4j.configuration=svc-log4j.properties myapp.fibo.service.Main &
 
-[[ $?==0 ]] && echo "The fibo server get started successfully. View log at ../logs"
+[[ $?==0 ]] && echo "The fibo service get started. View log at /var/log/fiboservice.log"
